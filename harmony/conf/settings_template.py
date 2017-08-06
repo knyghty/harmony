36933b50777d35a447730185d872b788f89de003
Tom Carrick <knyght@knyg.ht>
2017-08-06 22:15:56 +0100

    Add image spam command

--- harmony/conf/settings_template.py
+++ harmony/conf/settings_template.py
@@ -1,3 +1,7 @@
 # Rename this file to settings.py and edit the settings however you need.
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 BOT_TOKEN = ''  # Discord bot OAuth token
---
+++ harmony/extensions/images.py
@@ -0,0 +1,36 @@
+import glob
+import os
+
+from discord.ext import commands
+
+from conf import settings
+
+
+IMAGE_DIR = os.path.join(settings.BASE_DIR, 'media', 'images')
+
+
+class Image:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def i(self, image: str):
+        """
+        Upload an image with the given filename.
+
+        Keyword arguments:
+        filename -- File name of the file to upload
+        """
+        print(os.path.join(IMAGE_DIR, image + '.*'))
+        matches = glob.glob(os.path.join(IMAGE_DIR, image + '.*'))
+        print(matches)
+        if len(matches) != 1:
+            await self.bot.say('uwotm8')
+            return
+
+        await self.bot.upload(matches[0])
+
+
+
+def setup(bot):
+    bot.add_cog(Image(bot))
