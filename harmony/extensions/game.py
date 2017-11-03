72f79d0012f93ce69199e96fd75c2c324d110cbd
Mauro Vieira <mauro.vieira@gmail.com>
2017-11-03 11:34:12 -0300

    Fixes from review

--- harmony/extensions/game.py
+++ harmony/extensions/game.py
@@ -55,7 +55,7 @@ class Game:
         extra: str = ''
         if words:
             extra = ' You said: {}'.format(words)
-        await self.bot.say('You\'re {}.{}'.format(ctx.message.author, extra))
+        await self.bot.say("You're {}.{}".format(ctx.message.author, extra))
 
     @commands.command(pass_context=True)
     async def makechar(self, ctx):
@@ -69,7 +69,7 @@ class Game:
 
         session.add(player)
         session.commit()
-        await self.bot.say('```\n{}\'s {} player:\nTOUGHNESS: {}\nINTELLIGENCE: {}\nTACT: {}\nSPEED: {}```'.format(
+        await self.bot.say("```\n{}'s {} player:\nTOUGHNESS: {}\nINTELLIGENCE: {}\nTACT: {}\nSPEED: {}```".format(
             player.user_name,
             status,
             player.toughness,
@@ -84,7 +84,7 @@ class Game:
         try:
             player = session.query(Player).filter(Player.user_id == ctx.message.author.id)[0]
         except IndexError:
-            await self.bot.say('You don\'t have a character. Make one with `!makechar`.')
+            await self.bot.say("You don't have a character. Make one with `!makechar`.")
             return
 
         if player.hit_points <= 0 or player.enemy_hit_points <= 0:
@@ -103,13 +103,12 @@ class Game:
         combatant = '{:30s} {:2d}'
         content = [
             '```',
-            combatant.format('{}'.format(player.user_name), player.hit_points),
+            combatant.format(player.user_name, player.hit_points),
             combatant.format('Monster', player.enemy_hit_points),
             '```'
         ]
         if player.user_id in self.battles:
-            message = self.battles[player.user_id]
-            message = await self.bot.edit_message(message, '\n'.join(content))
+            message = await self.bot.edit_message(self.battles[player.user_id], '\n'.join(content))
         else:
             message = await self.bot.say('\n'.join(content))
         self.battles[player.user_id] = message
